156b5fbb58143dfef4803e2b6ef746401d74411c
Satej Dhanani <dhananisatej@gmail.com>
2024-08-06 21:20:08 -0700

    hopefully i can get this done soon

---
+++ README.md
@@ -0,0 +1,13 @@
+# Writing a very Spooky Spelling Game!
+
+### This is going to be where i document my intentions for the browser game that i will be creating. 
+This game is called *Spooky Man* and it will be spelling game that, if you lose a ghost will never find a person to scare! something like that atleast. 
+
+
+
+I am going to be writing this file in 3 different files. 
+* JavaScript (App.js)
+* HTML (index.html)
+* Css (style.css)
+
+in Javascript i will be utilizing a couple different functions in order to get a players timer, the array that will contain the random words, and many other details that the user is going to be able to see while playing the game. 
---
+++ js/app.js
@@ -0,0 +1,39 @@
+// okay heres where the real deal will start 
+/*
+=============Constants=================
+ i think therer are going to be hella consts im going to make here 
+ - const keyboard;
+ - const timer 
+ - const challengerWord
+ - const retry 
+ - const playerGuess
+ - const wordArray = [
+    { word: example 
+     hint: something to help them out maybe?
+    }
+    ]
+- const maxGuess = 10 or someething 
+- const timeLimit = 1000 seconds or something 
+
+ ============Lets======================
+ - let activeWord 
+ - let correctLetters
+ - let incorrectCount 
+ - let timerSeconds
+=============functions ================
+function resetGame() to clear the board 
+
+function getRandomWord () to grab from the wordArray 
+
+function createTimer() this will be pretty easy to implement just starting the timer we set in the const tab 
+
+function endGame() this will be to check if the player has either has won or lost the game 
+
+function startGame() {
+this will contain pretty much all the other functions to start this}
+
+
+
+
+
+*/
